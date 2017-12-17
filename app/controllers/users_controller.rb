db3a0cb44ef0c662387aee77d648c0e9c12707b3
Sarawuth <sarawuth@ampostech.com>
2017-12-17 22:07:44 +0700

    Fix signin upon singup bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to user_path(@user)
     else
